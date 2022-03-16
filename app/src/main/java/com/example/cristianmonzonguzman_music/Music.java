062ab662a928e20583ce4f99f5e26e894009ec6d
Cristian Monzon Guzman <Cristian-02-@live.com>
2022-03-16 14:03:18 -0500

    Add the listView

--- app/src/main/java/com/example/cristianmonzonguzman_music/Music.java
+++ app/src/main/java/com/example/cristianmonzonguzman_music/Music.java
@@ -1,14 +1,124 @@
 package com.example.cristianmonzonguzman_music;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Music extends AppCompatActivity {
+
+    // Declare the buttons
+    Button btnBack;
+
+    // Declare ListView
+    ListView ltList;
+
+    // Static String music
+    static String music = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        // Set ListView
+        ltList = findViewById(R.id.ltList);
+        btnBack = findViewById(R.id.btnBack);
+
+        // Get the parameter
+        music = getIntent().getStringExtra("music");
+
+        // Define the method OnClick
+        btnBack.setOnClickListener(view -> Callback());
+
+        // Valid the parameter
+        switch (music) {
+
+            case "classic":
+                ChargeClassic();
+                break;
+
+            case "rock":
+                ChargeRock();
+                break;
+
+            case "funk":
+                ChargeFunk();
+                break;
+
+            case "jazz":
+                ChargeJazz();
+                break;
+
+            case "pop":
+                ChargePop();
+                break;
+
+            default:
+        }
+
+
+    }
+
+    // Define the methods
+
+    private void Callback() {
+
+        Intent vBack = new Intent(Music.this, Task.class);
+        startActivity(vBack);
+
+    }
+
+    private void ChargeClassic() {
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(Music.this, R.array.classic_music,
+                        android.R.layout.simple_list_item_1);
+
+        ltList.setAdapter(adapter);
+
+    }
+
+    private void ChargeRock() {
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(Music.this, R.array.rock_music,
+                        android.R.layout.simple_list_item_1);
+
+        ltList.setAdapter(adapter);
+
+    }
+
+    private void ChargeFunk() {
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(Music.this, R.array.funk_music,
+                        android.R.layout.simple_list_item_1);
+
+        ltList.setAdapter(adapter);
+
+    }
+
+    private void ChargeJazz() {
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(Music.this, R.array.jazz_music,
+                        android.R.layout.simple_list_item_1);
+
+        ltList.setAdapter(adapter);
+
+    }
+
+    private void ChargePop() {
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(Music.this, R.array.pop_music,
+                        android.R.layout.simple_list_item_1);
+
+        ltList.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/cristianmonzonguzman_music/Task.java
+++ app/src/main/java/com/example/cristianmonzonguzman_music/Task.java
@@ -1,12 +1,11 @@
 package com.example.cristianmonzonguzman_music;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Task extends AppCompatActivity {
 
@@ -30,66 +29,26 @@ public class Task extends AppCompatActivity {
         setContentView(R.layout.activity_task);
 
         // Set the buttons
-        btnBack = (Button) findViewById(R.id.btnBack);
-        btnClassic = (ImageButton) findViewById(R.id.btnClassic);
-        btnRock = (ImageButton) findViewById(R.id.btnRock);
-        btnFunk = (ImageButton) findViewById(R.id.btnFunk);
-        btnJazz = (ImageButton) findViewById(R.id.btnJazz);
-        btnPop = (ImageButton) findViewById(R.id.btnPop);
+        btnBack = findViewById(R.id.btnBack);
+        btnClassic = findViewById(R.id.btnClassic);
+        btnRock = findViewById(R.id.btnRock);
+        btnFunk = findViewById(R.id.btnFunk);
+        btnJazz = findViewById(R.id.btnJazz);
+        btnPop = findViewById(R.id.btnPop);
 
         // Define the methods OnClick
-        btnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CallBack();
-            }
-        });
+        btnBack.setOnClickListener(view -> CallBack());
 
         // Define the Methods OnClick
-        btnClassic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnClassic.setOnClickListener(view -> ChargeClassic());
 
-                ChargeClassic();
+        btnRock.setOnClickListener(view -> ChargeRock());
 
-            }
-        });
+        btnFunk.setOnClickListener(view -> ChargeFunk());
 
-        btnRock.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btnJazz.setOnClickListener(view -> ChargeJazz());
 
-                ChargeRock();
-
-            }
-        });
-
-        btnFunk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ChargeFunk();
-
-            }
-        });
-
-        btnJazz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ChargeJazz();
-
-            }
-        });
-
-        btnPop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ChargePop();
-
-            }
-        });
+        btnPop.setOnClickListener(view -> ChargePop());
 
 
     }
